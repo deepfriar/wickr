a8495c6ff913085dd4384417213836978312c037
Gordon Arsenoff <deep.friar@gmail.com>
2020-12-30 13:40:22 -0500

    bank

--- R/amce.R
+++ R/amce.R
@@ -7,20 +7,36 @@ tidify.amce <- function(x, margins=FALSE, .sdiff=FALSE, ...) {
   foo <- Reduce(cbind, x$cond.estimates)
   bar <- Reduce(cbind, x$estimates)
 
-  foo[, colnames(bar)] <- bar # egad this is inelegant
+  # foo[, colnames(bar)] <- bar # egad this is inelegant
+  foo <- cbind(bar, foo[, setdiff(colnames(foo), colnames(bar))]) # this is worse
 
   foo <- reshape2::dcast(reshape2::melt(foo), Var2 ~ Var1)
 
   colnames(foo) <- c("term", "estimate", "std.error")
 
-  foo$outcome <- ifelse(stringr::str_detect(foo$term, x$respondent.varying), "Difference", "Average Effect")
+  foo$statistic <- foo$estimate / foo$std.error
+  foo$p.value   <- 2 * stats::pnorm(abs(foo$statistic), lower.tail=FALSE) # package cjoint prescribes pnorm, not pt
+
+  foo$outcome <- ifelse(sapply(stringr::str_detect(foo$term, as.character(x$respondent.varying)[1]), isTRUE),
+                        "Difference", "Average Effect")
   foo$term <- qdap::multigsub(paste0("^(.*:)?", foo$term[foo$outcome=="Average Effect"], "(:.*)?$"),
                               foo$term[foo$outcome=="Average Effect"], foo$term, fixed=FALSE)
 
   foo$level <- unlist(x$user.levels)[as.character(foo$term)] # ffs
-  foo$term  <- qdap::multigsub(qdap::multigsub(names(x$user.names),  "", foo$term), "", foo$term)
+  # foo$term  <- qdap::multigsub(qdap::multigsub(names(x$user.names),  "", foo$term), "", foo$term)
 
-  foo$level[stringr::str_detect(foo$term, x$respondent.varying)] <- "(Intercept)"
+  quux <- x$attributes
+  quux <- lapply(quux, list)
+  quux <- dplyr::as_tibble(quux)
+  quux <- tidyr::gather(quux, "newterm", "oldlevel")
+  quux <- tidyr::unnest(quux, .data$oldlevel)
+  quux <- dplyr::mutate(quux, term = paste0(.data$newterm, .data$oldlevel))
+
+  foo <- dplyr::left_join(foo, quux)
+  foo <- dplyr::mutate(foo, term = ifelse(is.na(.data$newterm), .data$term, .data$newterm))
+  foo <- dplyr::select(foo, -.data$newterm, -.data$oldlevel)
+
+  foo$level[sapply(stringr::str_detect(foo$term, as.character(x$respondent.varying)[1]), isTRUE)] <- "(Intercept)"
 
   ## the conditional differences
 
@@ -30,10 +46,25 @@ tidify.amce <- function(x, margins=FALSE, .sdiff=FALSE, ...) {
 
   # baz <- plyr::ldply(list(Main="estimates", Diff="cond.estimates"), .amce_tidify, x=x, .id="component")
 
-  foo$statistic <- foo$estimate / foo$std.error
-  foo$p.value   <- 2 * stats::pnorm(abs(foo$statistic), lower.tail=FALSE) # package cjoint prescribes pnorm, not pt
+  baz <- x$baselines
+  baz <- dplyr::as_tibble(baz)
+  baz <- tidyr::gather(baz, "term", "baseline")
 
-  foo[, c("outcome", "term", "level", setdiff(colnames(foo), c("outcome", "term", "level")))]
+  corge <- x$user.levels
+  corge <- dplyr::as_tibble(corge)
+  corge <- tidyr::gather(corge, "oldterm", "newlevel")
+  corge <- dplyr::left_join(corge, dplyr::select(quux, oldterm = .data$term, term = .data$newterm, .data$oldlevel))
+  corge <- dplyr::select(corge, -.data$oldterm)
+  corge <- dplyr::rename(corge, baseline = .data$oldlevel)
+
+  baz <- dplyr::left_join(baz, corge)
+  baz <- dplyr::mutate(baz, baseline = ifelse(is.na(.data$newlevel), .data$baseline, .data$newlevel))
+  baz <- dplyr::select(baz, -.data$newlevel)
+
+  foo <- dplyr::left_join(foo, baz)
+  foo <- dplyr::mutate(foo, baseline = ifelse(is.na(.data$baseline), "", .data$baseline))
+
+  foo[, c("outcome", "term", "level", "baseline", setdiff(colnames(foo), c("outcome", "term", "level", "baseline")))]
 }
 
 # .amce_tidify <- function(x, i) {
